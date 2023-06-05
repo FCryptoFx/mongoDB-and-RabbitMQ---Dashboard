4fb681f24211b618ec73306bbfa445adc540b3ac
fbadia <fbadia@grupokh.com>
2023-06-05 14:21:32 +0200

    added live monitoring graph for RabbitMQ queues data

--- main.py
+++ main.py
@@ -14,6 +14,9 @@ from dotenv import load_dotenv
 
 import requests
 
+from datetime import datetime
+from collections import deque
+
 # Load environment variables from the .env file
 load_dotenv()
 
@@ -64,22 +67,12 @@ def get_Collections_Number():
     return data
 
 
-# Función para actualizar el gráfico
 @app.callback(
     Output('graph-colecciones', 'figure'),
     Input('interval-colecciones', 'n_intervals')
 )
-def upddate_Collections_Data(n_intervals):
+def update_Collections_Data(n_intervals):
     data = get_Collections_Number()
-
-    """fig1 = {
-        'data': data,
-        'layout': {
-            'title': 'Colections Activity',
-            'xaxis': {'title': ''},
-            'yaxis': {'title': 'Transactions'}
-        }
-    }"""
 
     fig1 = {
         'data': data,
@@ -89,11 +82,34 @@ def upddate_Collections_Data(n_intervals):
                 'x': 0.5,  # Alinear el título al centro del gráfico
                 'font': {'size': 24, 'color': 'black', 'family': 'Arial'}
             },
-            'xaxis': {'title': '', 'tickfont': {'size': 14}},
-            'yaxis': {'title': 'Transactions', 'tickfont': {'size': 10}},
-            'plot_bgcolor': 'lightgray'  # Cambiar el color de fondo del área de trazado a gris claro
+            'xaxis': {
+                'title': '',
+                'tickfont': {'size': 14},
+                'linecolor': 'black',  # Color de la línea del eje x
+                'linewidth': 1,  # Grosor de la línea del eje x
+                'ticks': 'outside',  # Posición de las marcas de los ejes
+                'tickcolor': 'black',  # Color de las marcas de los ejes
+                'tickwidth': 1,  # Grosor de las marcas de los ejes
+                'ticklen': 8  # Longitud de las marcas de los ejes
+            },
+            'yaxis': {
+                'title': 'Transactions',
+                'tickfont': {'size': 10},
+                'linecolor': 'black',  # Color de la línea del eje y
+                'linewidth': 1,  # Grosor de la línea del eje y
+                'ticks': 'outside',  # Posición de las marcas de los ejes
+                'tickcolor': 'black',  # Color de las marcas de los ejes
+                'tickwidth': 1,  # Grosor de las marcas de los ejes
+                'ticklen': 8  # Longitud de las marcas de los ejes
+            },
+            'plot_bgcolor': 'white',  # Cambiar el color de fondo del área de trazado a blanco
+            'paper_bgcolor': 'rgba(0, 0, 0, 0)',  # Hacer transparente el color de fondo del área del gráfico
+            'margin': {'t': 80, 'b': 80, 'l': 80, 'r': 80}  # Ajustar los márgenes del gráfico
         }
     }
+
+    return fig1
+
 
     return fig1
 
@@ -144,12 +160,11 @@ df = get_Transactions_Data()
 
 # Crear la figura de la gráfica de barras
 fig = go.Figure(data=[go.Bar(x=df['Fecha'], y=df['Documentos'])])
-fig.update_traces(marker_color='rgb(255, 0, 0)')  # Cambiar el color de las barras a rojo
+fig.update_traces(marker_color='rgb(74, 205,141)')  # Cambiar el color de las barras a rojo
 fig.update_traces(marker_line_width=5)  # Cambiar el grosor de las líneas de las barras a 2
-fig.update_traces(marker_line_color='rgb(0, 0, 255)', marker_line_width=1)  # Cambiar el color a azul y el grosor a 1
+fig.update_traces(marker_line_color='rgb(7,20,14)', marker_line_width=1)  # Cambiar el color a azul y el grosor a 1
 fig.update_layout(
     title={'text': 'Transactions per day', 'x': 0.5},
-    xaxis_title='Date',
     yaxis_title='Transactions',
     title_font=dict(family='Arial', size=24, color='black'),
     xaxis=dict(title_font=dict(family='Arial', size=14, color='rgb(0, 0, 0)')),
@@ -157,7 +172,7 @@ fig.update_layout(
 )
 
 
-############# RABBITMQ DATA ##################
+############# RABBITMQ QUEUE DATA ##################
 def get_queue_info():
     url = api_url + 'api/queues'
     response = requests.get(url, auth=auth)
@@ -167,6 +182,29 @@ def get_queue_info():
     else:
         return []
 
+
+@app.callback(Output('queue-info-output', 'children'), [Input('interval-component', 'n_intervals')])
+def update_queue_info(n):
+    queue_info = get_queue_info()
+    if queue_info:
+        table_header = [html.Tr([html.Th('Queue name', style={'font-size': '35px'}), html.Th('Queued messages', style={'font-size': '35px'})], style={'text-align': 'center'})]
+        table_rows = [html.Tr([html.Td(queue['name'], style={'font-size': '70px'}), html.Td(queue['messages'], style={'font-size': '70px'})], style={'text-align': 'center'}) for queue in queue_info]
+        table = html.Table(table_header + table_rows, style={'margin': 'auto'})
+        return table
+    else:
+        return html.Div("No se pudo obtener información de las colas.", style={'text-align': 'center'})
+
+############# RABBITMQ QUEUE DATA GRAPH MONITORING ##################
+# Establecer el tamaño máximo del histórico de datos
+max_data_points = 100
+
+# Crear deque para almacenar los datos históricos
+data_points = deque(maxlen=max_data_points)
+
+# Crear la figura inicial del gráfico lineal
+fig2 = go.Figure()
+
+# Definir la función para obtener información general
 def get_overview_info():
     url = api_url + 'api/overview'
     response = requests.get(url, auth=auth)
@@ -176,16 +214,55 @@ def get_overview_info():
     else:
         return []
 
-@app.callback(Output('queue-info-output', 'children'), [Input('interval-component', 'n_intervals')])
-def update_queue_info(n):
-    queue_info = get_queue_info()
-    if queue_info:
-        table_header = [html.Tr([html.Th('Nombre de la cola', style={'font-size': '35px'}), html.Th('Mensajes encolados', style={'font-size': '35px'})], style={'text-align': 'center'})]
-        table_rows = [html.Tr([html.Td(queue['name'], style={'font-size': '70px'}), html.Td(queue['messages'], style={'font-size': '70px'})], style={'text-align': 'center'}) for queue in queue_info]
-        table = html.Table(table_header + table_rows, style={'margin': 'auto'})
-        return table
-    else:
-        return html.Div("No se pudo obtener información de las colas.", style={'text-align': 'center'})
+# Definir la función de actualización del gráfico en tiempo real
+@app.callback(
+    Output('graph-component', 'figure'),
+    [Input('interval-component', 'n_intervals')]
+)
+def update_graph(n):
+    # Obtener los datos de estado de las colas
+    overview_info = get_overview_info()
+    if overview_info:
+        queue_totals = overview_info.get('queue_totals', {})
+        messages = queue_totals.get('messages', 0)
+        timestamp = datetime.now()
+
+        # Agregar el punto de datos actual a la lista
+        data_points.append((timestamp, messages))
+
+        # Actualizar los datos en el gráfico
+        fig2 = go.Figure(data=go.Scatter(
+            x=[dp[0] for dp in data_points],
+            y=[dp[1] for dp in data_points],
+            mode='lines',
+            name='Queued messages'
+        ))
+
+        # Obtener el máximo valor de mensajes encolados
+        max_messages = max([dp[1] for dp in data_points])
+
+        # Ajustar el rango del eje y
+        y_axis_range = [0, max_messages * 1.5]  # Ajustar el factor multiplicativo según tus necesidades
+
+        # Establecer el layout del gráfico
+        fig2.update_layout(
+            title={
+                'text': 'Queue Status',
+                'x': 0.5,  # Centrar el título horizontalmente
+                'y': 0.95  # Posición vertical del título
+            },
+            yaxis={
+                'title': 'Queued Messages',
+                'title_standoff': 20,  # Ajustar la separación entre el título y el eje y
+                'tickfont': {'size': 12},  # Ajustar el tamaño de las etiquetas del eje y
+                'range': y_axis_range # Establecer el rango del eje y
+            },
+            template='plotly_dark',
+            margin={'t': 35, 'b': 10}
+        )
+
+        return fig2
+
 
 
 """@app.callback(Output('overview-info-output', 'children'), [Input('interval-component', 'n_intervals')])
@@ -206,8 +283,6 @@ def update_overview_info(n):
         return html.Div("No se pudo obtener información general.", style={'text-align': 'center'})"""
 
 
-
-
 ##################################################################################################################
 #############################################  APP LAYOUT  #######################################################
 ##################################################################################################################
@@ -219,15 +294,20 @@ app.layout = html.Div(
         html.Div(
             className='graphs-container',
             children=[
-                dcc.Graph(id='graph-colecciones', className='graph'),
-                dcc.Graph(id='grafica-documentos', figure=fig, className='graph')
+                dcc.Graph(id='graph-colecciones', className='graph', style={'height': '500px'}),
+                dcc.Graph(id='grafica-documentos', figure=fig, className='graph', style={'height': '500px'})
             ]
         ),
         html.Div(
             className='info-container',
             children=[
-                html.Div(id='queue-info-output', className='info-item'),
-                #html.Div(id='overview-info-output', className='info-item')
+                html.Div(
+                    className='info-column',
+                    children=[
+                        html.Div(id='queue-info-output', className='info-item', style={'height': '250px'}),
+                        dcc.Graph(id='graph-component', className='info-item', style={'height': '250px', 'width': '600px'})
+                    ]
+                ),
             ]
         ),
         dcc.Interval(id='interval-colecciones', interval=5000, n_intervals=0),
